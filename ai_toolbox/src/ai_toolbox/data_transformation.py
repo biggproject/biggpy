81e000348685aea97817516b0228bd54f4f5330a
rdevivo74 <riccardodevivo@gmail.com>
2021-11-19 17:01:24 +0100

    completed function weekly_profile_detection, modified yearly_profile_detection, updated package to version 0.5.1, added tests (tox tests ok)

--- ai_toolbox/src/ai_toolbox/data_transformation.py
+++ ai_toolbox/src/ai_toolbox/data_transformation.py
@@ -24,10 +24,17 @@ def yearly_profile_detection(data, exclude_days=None):
             pattern of the input time series at daily frequency.
     """
 
-    def to_timestamp(day, month, year):
+    def to_timestamp(day, month, year, hour=0, minute=0, second=0, tz=pytz.utc):
         try:
-            return datetime(day=int(day), month=int(month), year=int(year), hour=0, minute=0, second=0, tzinfo=pytz.utc)
-        except ValueError as e:
+            return datetime(
+                day=int(day),
+                month=int(month),
+                year=int(year),
+                hour=int(hour),
+                minute=int(minute),
+                second=int(second),
+                tzinfo=tz)
+        except ValueError:
             return None
 
     # Check input data before computing the profile
@@ -66,11 +73,10 @@ def yearly_profile_detection(data, exclude_days=None):
     # Group data first by month and then by day and aggregate by median
     df_group = data.groupby(by=[data.index.month, data.index.day]).median()
 
-    # Set names of multiindex columns to indentify month and day
+    # Set names of multiindex columns to identify month and day
     df_group.index.set_names(["month", "day"], inplace=True)
 
     # Add column year and set it constant to last year for alignment
-    df_group["year"] = data.index.year[-1]
     df_group.reset_index(inplace=True)
 
     # Get datetime object from columns year, month and day applying to timestamp row wise
@@ -83,9 +89,107 @@ def yearly_profile_detection(data, exclude_days=None):
     # Drop rows with at least one NaT (number of days per month can be different over the years)
     df_group.dropna(axis=0, how='any', inplace=True)
 
-    # Reindex with the new "syntethic" DatetimeIndex and drop columns day, month, year
+    # Reindex with the new "synthetic" DatetimeIndex and drop columns day, month, year
     df_group.set_index("timestamp", drop=True, inplace=True)
-    df_group.drop(["month", "day", "year"], axis=1, inplace=True)
+    df_group.drop(["month", "day"], axis=1, inplace=True)
+
+    return df_group
+
+
+def weekly_profile_detection(data, exclude_days=None):
+    """
+    The function returns the weekly profile of the input time series.
+    It aggregates values of the input data over multiple years using
+    the median and return a time series at hourly resolution, aligned
+    with the last week of the series. The frequency of the input
+    data should not lower than 'H' (the maximum time step should be 'D').
+    The input series should cover at least two weeks of data.
+
+    :param data: Input time series whose weekly profile has to be detected.
+    :param exclude_days: Time series of days to exclude from the input time series.
+    :return: Time series representing the weekly profile, e.g. weekly electricity consumption
+            pattern of the input time series at hourly frequency.
+    """
+
+    def to_timestamp(day, month, year, hour=0, minute=0, second=0, tz=pytz.utc):
+        try:
+            return datetime(
+                day=int(day),
+                month=int(month),
+                year=int(year),
+                hour=int(hour),
+                minute=int(minute),
+                second=int(second),
+                tzinfo=tz)
+        except ValueError:
+            return None
+
+    # Check input data before computing the profile
+
+    if data.empty or (isinstance(data, pd.DataFrame) and data.shape[1] > 1):
+        raise ValueError("Input series must be not empty and have exactly one column (if DataFrame),"
+                         " i.e. shape = (n, 1).")
+
+    elif detect_time_step(data)[0] is None:
+        raise ValueError("Impossible to determine the frequency of the input time series.")
+
+    elif data.index.isocalendar().week.nunique() < 2:
+        raise ValueError("Input time series must cover at least two weeks to get a weekly profile.")
+
+    try:
+        frq_cmp = to_offset(detect_time_step(data)[0]) > to_offset('1H')
+        if frq_cmp:
+            raise ValueError("The frequency of the input time series must be not lower than '1H'.")
+    except TypeError:
+        raise ValueError("The frequency of the input time series must be not lower than '1H'.")
+
+    # Filter out holidays or other days from the input data
+    if exclude_days is not None:
+        if isinstance(exclude_days, pd.Series):
+            # Assuming exclude_days is a boolean series where True means filter out the day
+            # Get the datetimeindex of True values
+            exclude_days = exclude_days.index[exclude_days]
+            mask = pd.Series(data.index.normalize().isin(exclude_days), index=data.index)
+            data = data.mask(mask)
+        elif isinstance(exclude_days, list):
+            # If it is a list convert to utc datetimeindex before filtering
+            exclude_days = pd.to_datetime(exclude_days, utc=True)
+            mask = pd.Series(data.index.normalize().isin(exclude_days), index=data.index)
+            data = data.mask(mask)
+        else:
+            raise TypeError("exclude_days argument must be a list of a boolean series of days to exclude.")
+
+    # Group data first by month and then by day and aggregate by median
+    df_group = data.groupby(by=[data.index.dayofweek, data.index.hour]).median()
+
+    # Set names of multiindex columns to identify month and day
+    df_group.index.set_names(["dayofweek", "hour"], inplace=True)
+
+    # Get last Sunday in the original series and generate "synthetic" datetimeindex for alignment
+    last_sunday = data.index[data.index.dayofweek == 6][-1].replace(hour=23, minute=0, second=0, tzinfo=pytz.utc)
+    days = pd.date_range(end=last_sunday, freq='1H', periods=24 * 7)
+
+    # Add columns year, month, day and set it constant to last year, month for alignment and last week days for day
+    df_group["year"] = data.index.year[-1]
+    df_group["month"] = data.index.month[-1]
+    df_group["day"] = days.day
+
+    df_group.reset_index(inplace=True)
+
+    # Get datetime object from columns year, month and day applying to timestamp row wise
+    df_group["timestamp"] = df_group.apply(lambda x: to_timestamp(
+            hour=x["hour"],
+            day=x["day"],
+            month=x["month"],
+            year=x["year"]),
+        axis=1)
+
+    # Drop rows with at least one NaT (number of days per month can be different over the years)
+    df_group.dropna(axis=0, how='any', inplace=True)
+
+    # Reindex with the new "synthetic" DatetimeIndex and drop columns day, month, year
+    df_group.set_index("timestamp", drop=True, inplace=True)
+    df_group.drop(["dayofweek", "hour", "year", "month", "day"], axis=1, inplace=True)
 
     return df_group
 
@@ -95,3 +199,19 @@ if __name__ == '__main__':
     This module is not supposed to run as a stand-alone module.
     This part below is only for testing purposes. 
     """
+    from os.path import dirname, join, realpath, pardir
+    import matplotlib.pyplot as plt
+
+    # Load time series for profile (dataframe) from csv
+    dir_path = dirname(realpath(__file__))
+    filename = join(dir_path, pardir, pardir, "tests", "fixtures", "df_weekly_profile.csv")
+    profile_data = pd.read_csv(
+        filename,
+        sep=',',
+        parse_dates=True,
+        infer_datetime_format=True,
+        index_col=0)
+
+    profile = weekly_profile_detection(profile_data)
+    profile.plot(ylim=(15, 30), figsize=(10, 10)).grid(which='major', axis='both', linestyle='--')
+    plt.show()
--- ai_toolbox/tests/test_data_transformation.py
+++ ai_toolbox/tests/test_data_transformation.py
@@ -25,8 +25,17 @@ class DataTransformation(unittest.TestCase):
 
         # Load time series for profile (dataframe) from csv
         dir_path = dirname(realpath(__file__))
-        filename = join(dir_path, "fixtures", "data_for_profile.csv")
-        cls.df_profile = pd.read_csv(
+
+        filename = join(dir_path, "fixtures", "df_yearly_profile.csv")
+        cls.yearly_profile = pd.read_csv(
+            filename,
+            sep=',',
+            parse_dates=True,
+            infer_datetime_format=True,
+            index_col=0)
+
+        filename = join(dir_path, "fixtures", "df_weekly_profile.csv")
+        cls.weekly_profile = pd.read_csv(
             filename,
             sep=',',
             parse_dates=True,
@@ -34,8 +43,16 @@ class DataTransformation(unittest.TestCase):
             index_col=0)
 
         # Load resulting time series for profile (dataframe) from csv
-        filename = join(dir_path, "fixtures", "result_profile.csv")
-        cls.result_profile = pd.read_csv(
+        filename = join(dir_path, "fixtures", "df_yearly_profile_result.csv")
+        cls.result_yearly_profile = pd.read_csv(
+            filename,
+            sep=',',
+            parse_dates=True,
+            infer_datetime_format=True,
+            index_col=0)
+
+        filename = join(dir_path, "fixtures", "df_weekly_profile_result.csv")
+        cls.result_weekly_profile = pd.read_csv(
             filename,
             sep=',',
             parse_dates=True,
@@ -49,6 +66,8 @@ class DataTransformation(unittest.TestCase):
             index=idx,
             columns=["n1", "n2"]
         )
+
+    # yearly_profile_detection tests below
 
     def test_yearly_profile_detection_raises_if_empty_series(self):
         """ Test that yearly_profile_detection raises ValueError in case of empty time series """
@@ -68,7 +87,7 @@ class DataTransformation(unittest.TestCase):
         """ Test that yearly_profile_detection raises if the frequency cannot be determined """
 
         mock_detect_time_step.return_value = None, None
-        self.assertRaises(ValueError, data_transformation.yearly_profile_detection, self.df_profile)
+        self.assertRaises(ValueError, data_transformation.yearly_profile_detection, self.yearly_profile)
 
     @patch("ai_toolbox.data_transformation.detect_time_step")
     def test_yearly_profile_detection_raises_if_freq_lower_than_daily(self, mock_detect_time_step):
@@ -76,7 +95,7 @@ class DataTransformation(unittest.TestCase):
 
         # Try with yearly frequency
         mock_detect_time_step.return_value = 'Y', 'None'
-        self.assertRaises(ValueError, data_transformation.yearly_profile_detection, self.df_profile)
+        self.assertRaises(ValueError, data_transformation.yearly_profile_detection, self.yearly_profile)
 
     @patch("ai_toolbox.data_transformation.detect_time_step")
     def test_yearly_profile_detection_ok_if_freq_higher_than_daily(self, mock_detect_time_step):
@@ -88,15 +107,15 @@ class DataTransformation(unittest.TestCase):
         for frequency in ['60S', '1T', '30min', '15H', '1D']:
             mock_detect_time_step.return_value = frequency, 'None'
             assert_frame_equal(
-                data_transformation.yearly_profile_detection(self.df_profile),
-                self.result_profile,
+                data_transformation.yearly_profile_detection(self.yearly_profile),
+                self.result_yearly_profile,
                 check_exact=False,
                 check_dtype=False)
 
     def test_yearly_profile_detection_raises_if_exclude_days_wrong_type(self):
         """ Test that yearly_profile_detection raises if exclude_days is of the wrong type """
 
-        self.assertRaises(TypeError, data_transformation.yearly_profile_detection, self.df_profile, "wrong_type")
+        self.assertRaises(TypeError, data_transformation.yearly_profile_detection, self.yearly_profile, "wrong_type")
 
     def test_yearly_profile_detection_ok_if_exclude_days_list(self):
         """
@@ -104,9 +123,10 @@ class DataTransformation(unittest.TestCase):
         and does not raise exception
         """
 
-        df_result_exclude = self.result_profile.drop(pd.Timestamp("2021-03-17 00:00:00+00:00"), axis="index")
+        df_result_exclude = self.result_yearly_profile.drop(pd.Timestamp("2021-03-17 00:00:00+00:00"), axis="index")
         assert_frame_equal(
-            data_transformation.yearly_profile_detection(self.df_profile, ["2019-03-17", "2020-03-17", "2021-03-17"]),
+            data_transformation.yearly_profile_detection(
+                self.yearly_profile, ["2019-03-17", "2020-03-17", "2021-03-17"]),
             df_result_exclude,
             check_exact=False,
             check_dtype=False)
@@ -119,9 +139,98 @@ class DataTransformation(unittest.TestCase):
 
         idx = pd.date_range(start='2019/01/01', end='2022/01/01', tz=pytz.utc, freq='D')
         series_exclude = pd.Series(data=((idx.day == 17) & (idx.month == 3)), index=idx)
-        df_result_exclude = self.result_profile.drop(pd.Timestamp("2021-03-17 00:00:00+00:00"), axis="index")
+        df_result_exclude = self.result_yearly_profile.drop(pd.Timestamp("2021-03-17 00:00:00+00:00"), axis="index")
         assert_frame_equal(
-            data_transformation.yearly_profile_detection(self.df_profile, series_exclude),
+            data_transformation.yearly_profile_detection(self.yearly_profile, series_exclude),
+            df_result_exclude,
+            check_exact=False,
+            check_dtype=False)
+
+    # weekly_profile_detection tests below
+    
+    def test_weekly_profile_detection_raises_if_empty_series(self):
+        """ Test that weekly_profile_detection raises ValueError in case of empty time series """
+
+        self.assertRaises(ValueError, data_transformation.weekly_profile_detection, pd.DataFrame(data=[]))
+
+    def test_weekly_profile_detection_raises_if_more_columns(self):
+        """
+        Test that weekly_profile_detection raises ValueError in case of DataFrame with
+        more than one column
+        """
+
+        self.assertRaises(ValueError, data_transformation.weekly_profile_detection, self.df_two_columns)
+
+    @patch("ai_toolbox.data_transformation.detect_time_step")
+    def test_weekly_profile_detection_raises_if_freq_none(self, mock_detect_time_step):
+        """ Test that weekly_profile_detection raises if the frequency cannot be determined """
+
+        mock_detect_time_step.return_value = None, None
+        self.assertRaises(ValueError, data_transformation.weekly_profile_detection, self.weekly_profile)
+
+    @patch("ai_toolbox.data_transformation.detect_time_step")
+    def test_weekly_profile_detection_raises_if_freq_lower_than_hourly(self, mock_detect_time_step):
+        """ Test that weekly_profile_detection raises if the frequency is lower than hourly """
+
+        # Try with daily frequency
+        mock_detect_time_step.return_value = '1D', 'None'
+        self.assertRaises(ValueError, data_transformation.weekly_profile_detection, self.weekly_profile)
+
+    @patch("ai_toolbox.data_transformation.detect_time_step")
+    def test_weekly_profile_detection_ok_if_freq_higher_than_hourly(self, mock_detect_time_step):
+        """
+        Test that weekly_profile_detection gives expected result for each frequency higher than 1H
+        and does not raise exception
+        """
+
+        for frequency in ['60S', '1T', '30min', '1H']:
+            mock_detect_time_step.return_value = frequency, 'None'
+            assert_frame_equal(
+                data_transformation.weekly_profile_detection(self.weekly_profile),
+                self.result_weekly_profile,
+                check_exact=False,
+                check_dtype=False)
+
+    def test_weekly_profile_detection_raises_if_exclude_days_wrong_type(self):
+        """ Test that weekly_profile_detection raises if exclude_days is of the wrong type """
+
+        self.assertRaises(TypeError, data_transformation.weekly_profile_detection, self.weekly_profile, "wrong_type")
+
+    def test_weekly_profile_detection_ok_if_exclude_days_list(self):
+        """
+        Test that weekly_profile_detection gives expected result if exclude days is a list
+        and does not raise exception
+        """
+
+        # Exclude all the Sundays
+        exclude_days = \
+            ['2020-07-05 00:00:00+00:00', '2020-07-12 00:00:00+00:00', '2020-07-19 00:00:00+00:00',
+             '2020-07-26 00:00:00+00:00', '2020-08-02 00:00:00+00:00', '2020-08-09 00:00:00+00:00',
+             '2020-08-16 00:00:00+00:00', '2020-08-23 00:00:00+00:00', '2020-08-30 00:00:00+00:00',
+             '2020-09-06 00:00:00+00:00', '2020-09-13 00:00:00+00:00', '2020-09-20 00:00:00+00:00',
+             '2020-09-27 00:00:00+00:00']
+        df_result_exclude = self.result_weekly_profile.drop(
+            pd.date_range(start="2020-09-27", freq='1H', tz=pytz.utc, periods=24))
+        assert_frame_equal(
+            data_transformation.weekly_profile_detection(
+                self.weekly_profile, exclude_days=exclude_days),
+            df_result_exclude,
+            check_exact=False,
+            check_dtype=False)
+
+    def test_weekly_profile_detection_ok_if_exclude_days_series(self):
+        """
+        Test that weekly_profile_detection gives expected result if exclude days is a series
+        and does not raise exception
+        """
+
+        # Exclude all the Sundays
+        idx = pd.date_range(start='2020-07-01', end='2020-09-30', tz=pytz.utc, freq='H')
+        series_exclude = pd.Series(data=(idx.dayofweek == 6), index=idx)
+        df_result_exclude = self.result_weekly_profile.drop(
+            pd.date_range(start="2020-09-27", freq='1H', tz=pytz.utc, periods=24))
+        assert_frame_equal(
+            data_transformation.weekly_profile_detection(self.weekly_profile, series_exclude),
             df_result_exclude,
             check_exact=False,
             check_dtype=False)
