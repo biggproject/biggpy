179c0e8b35f1d531c719579086968e321e0933ad
rdevivo74 <riccardodevivo@gmail.com>
2021-12-28 10:14:45 +0100

    added blocking time series split to data_modelling.py, updated software version setup.py (tox tests ok)

--- ai_toolbox/src/ai_toolbox/data_modelling.py
+++ ai_toolbox/src/ai_toolbox/data_modelling.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
+from abc import ABC
 from os.path import isabs, splitext, dirname, isdir
 
-from numpy import mean, std
+from numpy import arange, mean, std
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import get_scorer
@@ -11,6 +11,26 @@ from sklearn.model_selection import GridSearchCV, BaseCrossValidator, cross_vali
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+
+
+class BlockingTimeSeriesSplit(BaseCrossValidator, ABC):
+    def __init__(self, n_splits):
+        self.n_splits = n_splits
+
+    def get_n_splits(self, X=None, y=None, groups=None):
+        return self.n_splits
+
+    def split(self, X, y=None, groups=None):
+        n_samples = len(X)
+        k_fold_size = n_samples // self.n_splits
+        indices = arange(n_samples)
+
+        margin = 0
+        for i in range(self.n_splits):
+            start = i * k_fold_size
+            stop = start + k_fold_size
+            mid = int(0.5 * (stop - start)) + start
+            yield indices[start: mid], indices[mid + margin: stop]
 
 
 class PolynomialRegression(BaseEstimator, RegressorMixin):
